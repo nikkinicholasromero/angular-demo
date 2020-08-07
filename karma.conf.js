a7dbc41d58d6c240abe6a0e03bc838bb031bb4e8
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-08-07 19:27:50 -0400

    Fixed failing code coverage threshold check

--- karma.conf.js
+++ karma.conf.js
@@ -20,10 +20,10 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
-        statements: 90,
-        lines: 90,
-        branches: 90,
-        functions: 90
+        statements: 95,
+        lines: 95,
+        branches: 95,
+        functions: 95
       }
     },
     reporters: ['progress', 'kjhtml'],
